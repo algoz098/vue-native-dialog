eb27e9c09d5f4265be880d8b9977a1fd2623bc19
asena <asena@pinterest.com>
2022-03-17 17:55:31 -0300

    Remove embbed documentation

--- docs-src/.vitepress/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    base: `https://algoz098.github.io/vue-native-dialog`
-}
---
+++ docs/.vitepress/config.js
@@ -0,0 +1,7 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+const config = {
+}
+
+export default config
