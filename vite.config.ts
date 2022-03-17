93ac3c92b76d73d3a44756b126484c08e6653b31
asena <asena@pinterest.com>
2022-03-17 18:10:03 -0300

    Update build commands

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const path = require('path');
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/components/index.js'),
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
       name: 'vue-native-dialog',
       fileName: (format) => `vue-native-dialog.${format}.js`,
     },
